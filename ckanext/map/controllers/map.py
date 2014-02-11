d51ec75ff1143a4550c9b61efadd2437ad11c58c
vagrant <ben@benscott.co.uk>
2014-02-11 18:36:09 +0000

    CKAN integratiom

--- ckanext/map/controllers/map.py
+++ ckanext/map/controllers/map.py
@@ -7,6 +7,7 @@ from ckan.common import OrderedDict, _, json, request, c, g, response
 from ckan.lib.jsonp import jsonpify
 import logging
 import requests
+import pylons
 import urllib
 import cStringIO
 import ckanext.map.lib.helpers as helpers
@@ -52,41 +53,62 @@ class MapController(base.BaseController):
                   }
                   """
 
-    windshaft_base = 'http://10.11.12.1:4000/database/nhm_botany/table/botany_all/{z}/{x}/{y}'
+    windshaft_base = 'http://127.0.0.1:4000/database/nhm_botany/table/botany_all/{z}/{x}/{y}'
+
+    def __before__(self, action, **params):
+        """
+        Overriding __before__ to set up some db engine
+        """
+        super(MapController, self).__before__(action, **params)
+
+        # Get the datastore write URL
+        connection_url = pylons.config['ckan.datastore.write_url']
+
+        # Create the engine
+        self.engine = create_engine(connection_url)
+        self.windshaft_host = pylons.config['map.windshaft.host']
+        self.windshaft_port = pylons.config['map.windshaft.port']
+        # If a database has been set in the config, use that. Otherwise use the datastore
+        self.windshaft_database = pylons.config.get('map.windshaft.database', None) or self.engine.url.database
+
+        self.resource_id = request.params.get('resource_id')
+        self.filters = request.params.get('filters')
+        self.query = request.params.get('q')
+        self.geom = request.params.get('geom')
+        self.heatmap = request.params.get('heatmap')
+
+        # context = {'model': model, 'session': model.Session, 'user': c.user or c.author}
+
+        # Try & get the resource
+        # TODO: We could speed it up here by not checking if the resource exists? Using reflection checks anyway. But what about permissions?
+        # TODO: Otherwise, cache the lookup? Probably best - we need resource settings for heatmap etc., Test this
+        # try:
+        #     c.resource = get_action('resource_show')(context, {'id': resource_id})
+        # except NotFound:
+        #     abort(404, _('Resource not found'))
+        # except NotAuthorized:
+        #     abort(401, _('Unauthorized to read resources'))
+
+        self.geo_table = self.geo_table(self.resource_id)
+
+
+    def base_url(self):
+        return 'http://{}:{}/database/{}/table/{}/{{z}}/{{x}}/{{y}}'.format(self.windshaft_host, self.windshaft_port, self.windshaft_database, self.resource_id)
 
     def tile_url(self):
-      return self.windshaft_base + '.png?sql={sql}&style={style}'
+      return self.base_url() + '.png?sql={sql}&style={style}'
 
     def grid_url(self):
-      return self.windshaft_base + '.grid.json?callback={cb}&sql={sql}'
+      return self.base_url() + '.grid.json?callback={cb}&sql={sql}'
 
-    def geo_table(self):
+    def geo_table(self, resource_id):
       """ Return the SQLAlchemy table corresponding to the geo table in Windshaft
 
       Currently windshaft is configured with a slightly misleading table name of 'botany_all' containing
       all records, not just botany ones.
       """
       metadata = MetaData()
-      table = Table('botany_all', metadata,
-            Column('_id', Integer),
-            Column('type', String),
-            Column('collection_department', String),
-            Column('collection_sub_department', String),
-            Column('catalogue_number', String),
-            Column('scientific_name', String),
-            Column('genus', String),
-            Column('subgenus', String),
-            Column('species', String),
-            Column('scientific_name_author', String),
-            Column('continent', String),
-            Column('country', String),
-            Column('state_province', String),
-            Column('county', String),
-            Column('expedition_name', String),
-            Column('vessel_name', String),
-            Column('the_geom_webmercator', helpers.Geometry)
-      )
-      return table
+      return Table(resource_id, metadata, autoload=True, autoload_with=self.engine)
 
     def tile(self, z, x, y):
 
@@ -94,53 +116,37 @@ class MapController(base.BaseController):
         View a map tile
         :return: png image
         """
-
-        # URL in the format http://10.11.12.13:5000/map-tile/2/2/2.png?resource_id=64351390-720f-4702-bd25-9fa607629b3f
-
-        resource_id = request.params.get('resource_id')
-        filters = request.params.get('filters')
-        query = request.params.get('q')
-        geom = request.params.get('geom')
-        heatmap = request.params.get('heatmap')
-
-        context = {'model': model, 'session': model.Session, 'user': c.user or c.author}
-
-        # Try & get the resource
-        try:
-            c.resource = get_action('resource_show')(context, {'id': resource_id})
-        except NotFound:
-            abort(404, _('Resource not found'))
-        except NotAuthorized:
-            abort(401, _('Unauthorized to read resources'))
-
-        engine = create_engine('postgresql://')
-
-        geo_table = self.geo_table()
-
-        dep = c.resource['name']
-
-        s = select([geo_table]).where(geo_table.c.collection_department==dep)
-
-        if filters:
-          for filter in json.loads(filters):
+        # Build the query
+        s = select([self.geo_table])
+        if self.filters:
+            for filter in json.loads(self.filters):
             # TODO - other types of filters
-            if (filter['type'] == 'term'):
-              s = s.where(geo_table.c[filter['field']]==filter['term'])
+                if (filter['type'] == 'term'):
+                    s = s.where(self.geo_table.c[filter['field']] == filter['term'])
 
-        if geom:
-          s = s.where(geoFunctions.intersects(geo_table.c.the_geom_webmercator,geoFunctions.transform(geom,3857)))
-
-        sql = helpers.interpolateQuery(s, engine)
+        if self.geom:
+            s = s.where(geoFunctions.intersects(self.geo_table.c.the_geom_webmercator,geoFunctions.transform(self.geom,
+                                                                                                             3857)))
+        sql = helpers.interpolateQuery(s, self.engine)
 
         style = ''
 
-        if heatmap:
+        if self.heatmap:
           style = urllib.quote_plus(self.heatmap_mss)
 
         url = self.tile_url().format(z=z,x=x,y=y,sql=sql,style=style)
+        raw = urllib.urlopen(url).read()
+
+        # Quick and dirty check for an error
+        if 'ERROR' in raw:
+            # TODO: Getting the whole CKAN 404 page. Not needed for an image
+            abort(404, _('Resource not found'))
+            # Log the error extracted from the JSON response
+            data = json.loads(raw)
+            log.error(data['error'])
+
         response.headers['Content-type'] = 'image/png'
-        tile =  cStringIO.StringIO(urllib.urlopen(url).read())
-        return tile
+        return cStringIO.StringIO(raw)
 
     def grid(self, z, x, y):
 
