d302666c625b7e9c4b4803d5be6fc282830d3f8f
Alice Butcher <alycejenni@gmail.com>
2018-04-09 16:40:42 +0100

    manual changes to lib; removed Geometry class
    could not find any references to the helpers.Geometry class after commit #f7c56e, so I removed it

--- ckanext/tiledmap/lib/helpers.py
+++ ckanext/tiledmap/lib/helpers.py
@@ -5,30 +5,6 @@
 # Created by the Natural History Museum in London, UK
 
 import re
-from sqlalchemy import func
-from sqlalchemy.types import UserDefinedType
-
-class Geometry(UserDefinedType):
-    ''' '''
-    def get_col_spec(self):
-        ''' '''
-        return u'GEOMETRY'
-
-    def bind_expression(self, bindvalue):
-        '''
-
-        :param bindvalue: 
-
-        '''
-        return func.ST_GeomFromText(bindvalue, type_=self)
-
-    def column_expression(self, col):
-        '''
-
-        :param col: 
-
-        '''
-        return col
 
 
 # Template helpers
